42c6b255d181134f9277b689674b7465dc143bdc
Dima D <dergachovda@gmail.com>
2016-03-31 20:40:28 +0300

    add Factorial

---
+++ src/ua/in/dergachovda/less04/Factorial.java
@@ -0,0 +1,11 @@
+package ua.in.dergachovda.less04;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int n = 3;
+        int result = 1;
+        for (int i = 1; i <= n; i++)
+            result *= i;
+        System.out.println("result = " + result);
+    }
+}
