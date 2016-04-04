dc7e02524786162152bbf921b2af04386ee89c78
Dima D <dergachovda@gmail.com>
2016-04-04 19:52:55 +0300

    add cache

--- src/ua/in/dergachovda/less05/Main.java
+++ src/ua/in/dergachovda/less05/Main.java
@@ -3,33 +3,49 @@ package ua.in.dergachovda.less05;
 public class Main {
 
     public static void main(String[] args) {
+        int n = 100;
+        int[] cache = new int[n+1];
+
         long start = System.nanoTime();
-        int result = getSummaIterativety();
+        int result = getSummaIterativety(n);
+        System.out.println("The sum is: " + result);
         long end = System.nanoTime();
         long duration = end - start;
 
-        System.out.println("The sum is: " + result);
         System.out.println("Interative duration is: " + duration + " ns");
 
         start = System.nanoTime();
-        result = getSummaIterativety();
-        end = System.nanoTime();
-        duration = end - start;
-        
+        result = getFormula(n);
         System.out.println("The sum is: " + result);
-        System.out.println("Interative duration is: " + duration + " ns");
+        end = System.nanoTime();
+        cache[n] = result;
+        duration = end - start;
+
+        System.out.println("Formula duration is: " + duration + " ns");
+
+        if (cache[n] != 0) {
+            start = System.nanoTime();
+            System.out.println("cache = " + cache[n]);
+            end = System.nanoTime();
+            duration = end - start;
+        } else {
+            result = getFormula(n);
+            cache[n] = result;
+        }
+
+        System.out.println("Cache duration is: " + duration + " ns");
     }
 
-    public static int getSummaIterativety() {
+    public static int getSummaIterativety(int n) {
         int sum = 0;
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= n; i++) {
             sum += i;
         }
         return sum;
     }
 
-    public static int getFormula() {
-        return (1 + 100) * 100 / 2;
+    public static int getFormula(int n) {
+        return (1 + n) * n / 2;
     }
 
 }
