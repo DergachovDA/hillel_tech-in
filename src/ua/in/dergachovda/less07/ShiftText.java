9b4f3e966dff3b36d5bae9773fa642a0fa3efb65
Dima D <dergachovda@gmail.com>
2016-04-11 20:57:55 +0300

    add less07

---
+++ src/ua/in/dergachovda/less07/ShiftText.java
@@ -0,0 +1,21 @@
+package ua.in.dergachovda.less07;
+
+public class ShiftText {
+
+    public static void main(String[] args) {
+
+        byte number = 16;
+
+        number <<= 1;
+
+        number >>= 2;
+
+        System.out.println("number = " + number);
+
+        int test = 13;
+        test <<= 1;
+        System.out.println("test = " + test);
+
+    }
+
+}
